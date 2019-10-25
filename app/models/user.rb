39429e86710ef11907662ee45d1e4be3ff26b6b5
techspeterson <tpeterson.engsci@gmail.com>
2019-10-25 16:27:24 +1100

    model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :listings
+  has_many :wishlist_items
+  has_one :address
+  has_one :user_detail
 end
